8826630383720e544232d2c4c4e0808c2c848caf
Murtaza Udaipurwala <murtaza@murtazau.xyz>
2023-03-04 16:40:24 +0530

    Re-configured github actions
    Signed-off-by: Murtaza Udaipurwala <murtaza@murtazau.xyz>

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-      //common reveal options to create reveal animations
-      ScrollReveal({
-        //reset: true,
-        opacity: 0,
-        duration: 2500,
-        delay: 400,
-      });
+//common reveal options to create reveal animations
+ScrollReveal({
+  //reset: true,
+  opacity: 0,
+  duration: 2500,
+  delay: 400,
+});
 
-      //target elements, and specify options to create reveal animations
-      ScrollReveal().reveal(".card_01 , .card_03, .card_05, .card_07", {
-        delay: 500,
-        opacity: 1,
-      });
-      ScrollReveal().reveal(".card_02 , .card_04, .card_06, .card_08", {
-        delay: 500,
-        opacity: 1,
-      });
+//target elements, and specify options to create reveal animations
+ScrollReveal().reveal(".card_01 , .card_03, .card_05, .card_07", {
+  delay: 500,
+  opacity: 1,
+});
+ScrollReveal().reveal(".card_02 , .card_04, .card_06, .card_08", {
+  delay: 500,
+  opacity: 1,
+});
