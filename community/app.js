7481161c9639c5c3c97e21405901f6060d0671ea
Murtaza Udaipurwala <murtaza@murtazau.xyz>
2023-03-06 22:42:39 +0530

    Refactored community page into seperate directory
    Signed-off-by: Murtaza Udaipurwala <murtaza@murtazau.xyz>

---
+++ community/app.js
@@ -0,0 +1,30 @@
+//common reveal options to create reveal animations
+ScrollReveal({
+  //reset: true,
+  opacity: 0,
+  duration: 2500,
+  delay: 400,
+});
+
+//target elements, and specify options to create reveal animations
+ScrollReveal().reveal(".card_01 , .card_03, .card_05, .card_07", {
+  delay: 500,
+  opacity: 1,
+});
+ScrollReveal().reveal(".card_02 , .card_04, .card_06, .card_08", {
+  delay: 500,
+  opacity: 1,
+});
+
+const cardBody = document.querySelector('.card_body');
+
+// Check if the viewport width is less than or equal to 768px
+if (window.innerWidth <= 768) {
+  cardBody.addEventListener('mouseenter', function() {
+    this.classList.add('hovered');
+  });
+
+  cardBody.addEventListener('mouseleave', function() {
+    this.classList.remove('hovered');
+  });
+}
